7d84f8c236d38bae0c12e08b0f4b41f2fd1b7529
vladimir.burmistrov <vladimir.burmistrov@exactprosystems.com>
2024-08-26 01:04:36 +0400

    Rename print_name to hello_from

--- lesson001_ex003.py
+++ lesson001_ex003.py
@@ -2,10 +2,10 @@ class Me:
     def __init__(self, name):
         self._name = name
 
-    # print name
-    def print_name(self):
+    # print hello from Me
+    def hello_from(self):
         print("Hello from " + self._name)
 
 
 man_001 = Me("Vladimir")
-man_001.print_name()
+man_001.hello_from()
