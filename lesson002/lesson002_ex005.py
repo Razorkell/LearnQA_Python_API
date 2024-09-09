91f8484bf94e88f9ade63ff0a09309c05c7d6ff8
vladimir.burmistrov <vladimir.burmistrov@exactprosystems.com>
2024-09-09 20:15:47 +0400

    Erase spaces

--- lesson002/lesson002_ex005.py
+++ lesson002/lesson002_ex005.py
@@ -1,7 +1,8 @@
 import json
 from json import JSONDecodeError
 
-json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+json_text = ('{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And '
+             'this is a second message","timestamp":"2021-06-04 16:41:01"}]}')
 
 try:
     json_dict = json.loads(json_text)
