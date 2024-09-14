c62a063f77a7766dbd4fdba3796ee76a39017191
Razorkell <razorkell92@gmail.com>
2024-09-15 01:01:24 +0400

    Remove extra parameters

--- lesson003/lesson003_ex013.py
+++ lesson003/lesson003_ex013.py
@@ -4,30 +4,24 @@ import json
 
 result = []
 url = "https://gist.githubusercontent.com/KotovVitaliy/138894aa5b6fa442163561b5db6e2e26/raw/6916020a6a9cf1fbf0ee34c7233ade94d766cc96/some.txt"
-text = requests.get(url).text
-
-text1 = text.split('\n')
-text2 = text1
-
-len_text = len(text2)
+text = requests.get(url).text.split('\n')
 
 list_values = []
 user_agent_temp = ""
 expected_values_temp = ""
 
-for num_string, string in enumerate(text2):
+for num_string, string in enumerate(text):
     if string == '':
-        del text2[num_string]
-for num_string, string in enumerate(text2):
+        del text[num_string]
+for num_string, string in enumerate(text):
     if string == 'User Agent:':
-        user_agent_temp = text2[num_string + 1]
+        user_agent_temp = text[num_string + 1]
     elif string == 'Expected values:':
-        temp_string = '{' + text2[num_string + 1].replace('\'', '\"') + '}'
+        temp_string = '{' + text[num_string + 1].replace('\'', '\"') + '}'
         try:
             expected_values_temp = json.loads(temp_string)
         except json.JSONDecodeError:
             expected_values_temp = "Invalid json for expected value"
-        print(expected_values_temp)
         list_values.append({'User Agent': user_agent_temp, 'Expected values': expected_values_temp})
 
 print(list_values)
