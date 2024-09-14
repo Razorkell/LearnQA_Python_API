7ffaaf5aced2467e3f97bbcfd371943e0b2f97c8
Razorkell <razorkell92@gmail.com>
2024-09-14 16:55:38 +0400

    Add lesson003_ex011.py

---
+++ lesson003/lesson003_ex011.py
@@ -0,0 +1,84 @@
+import requests
+
+
+class TestCookies:
+
+    __page = "https://playground.learnqa.ru/api/homework_cookie"
+    __header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                              "AppleWebKit/537.36 (KHTML, like Gecko) "
+                              "Chrome/128.0.0.0 "
+                              "Safari/537.36"}
+    __cookies = [{"HomeWork": "hw_value"}]
+    __answers = {1: "Valid",
+                 0: "Cookies are blank",
+                 -1: "Response doesn't contain this cookie",
+                 -2: "Incorrect text in cookie"}
+
+    def set_page(self):
+        self.__page = input("Set a page for request: ")
+
+    def get_page(self):
+        return self.__page
+
+    def get_cookies(self):
+        return self.__cookies
+
+    def check_cookie(self, method):
+        __temp_response = requests.request(method, self.__page, headers=self.__header)
+        __temp_cookie = __temp_response.cookies
+        __result_code = -200
+        __result_cookie = []
+        if __temp_cookie is None:
+            __result_code = 0
+        else:
+            __temp_res = 0
+            for index, cookie in enumerate(self.__cookies):
+                for key_cookie in cookie:
+                    if key_cookie in __temp_cookie:
+                        __temp_res = 1
+                        __result_cookie = __temp_cookie
+                        if __temp_cookie[key_cookie] == cookie[key_cookie]:
+                            __result_code = 1
+                            break
+                        else:
+                            __result_code = -2
+                if __result_code == 1:
+                    break
+            if __temp_res == 0:
+                __result_code = -1
+        return __result_code, __result_cookie
+
+    def test_get_cookie(self):
+        __temp_result, __temp_cookie = TestCookies.check_cookie(self, 'GET')
+        assert __temp_result == 1, f"{self.__answers[0]}" if __temp_result == 0 \
+            else f"{self.__answers[__temp_result]}: {__temp_cookie}"
+
+    def test_post_cookie(self):
+        __temp_result, __temp_cookie = TestCookies.check_cookie(self, 'POST')
+        assert __temp_result == 1, f"{self.__answers[0]}" if __temp_result == 0 \
+            else f"{self.__answers[__temp_result]}: {__temp_cookie}"
+
+    def test_put_cookie(self):
+        __temp_result, __temp_cookie = TestCookies.check_cookie(self, 'PUT')
+        assert __temp_result == 1, f"{self.__answers[0]}" if __temp_result == 0 \
+            else f"{self.__answers[__temp_result]}: {__temp_cookie}"
+
+    def test_patch_cookie(self):
+        __temp_result, __temp_cookie = TestCookies.check_cookie(self, 'PATCH')
+        assert __temp_result == 1, f"{self.__answers[0]}" if __temp_result == 0 \
+            else f"{self.__answers[__temp_result]}: {__temp_cookie}"
+
+    def test_delete_cookie(self):
+        __temp_result, __temp_cookie = TestCookies.check_cookie(self, 'DELETE')
+        assert __temp_result == 1, f"{self.__answers[0]}" if __temp_result == 0 \
+            else f"{self.__answers[__temp_result]}: {__temp_cookie}"
+
+    def test_head_cookie(self):
+        __temp_result, __temp_cookie = TestCookies.check_cookie(self, 'HEAD')
+        assert __temp_result == 1, f"{self.__answers[0]}" if __temp_result == 0 \
+            else f"{self.__answers[__temp_result]}: {__temp_cookie}"
+
+    def test_options_cookie(self):
+        __temp_result, __temp_cookie = TestCookies.check_cookie(self, 'OPTIONS')
+        assert __temp_result == 1, f"{self.__answers[0]}" if __temp_result == 0 \
+            else f"{self.__answers[__temp_result]}: {__temp_cookie}"
