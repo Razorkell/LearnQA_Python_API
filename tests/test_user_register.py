8eadd247ac1949e078909fc71224ebe1368435c7
Razorkell <razorkell92@gmail.com>
2024-09-22 23:48:05 +0400

    Update allure descriptions

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -21,7 +21,7 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, "id")
 
-    @allure.description("This test checks denied user registration with already existed email")
+    @allure.description("This test checks invalid registration with already existed email")
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
         data = self.prepare_registration_data(email=email)
@@ -32,7 +32,7 @@ class TestUserRegister(BaseCase):
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", \
             f"Unexpected response content {response.content}"
 
-    @allure.description("This test checks registration of user with invalid email")
+    @allure.description("This test checks invalid registration of user with invalid email")
     def test_create_user_with_invalid_email(self):
         email = 'withoutaexample.com'
         data = self.prepare_registration_data(email=email)
@@ -42,7 +42,7 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"Invalid email format"
 
-    @allure.description("This test checks registration of user without any required field")
+    @allure.description("This test checks invalid registration of user without any required field")
     @pytest.mark.parametrize('field', required_fields)
     def test_create_user_without_field(self, field):
         data = self.prepare_registration_data()
@@ -53,7 +53,7 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"The following required params are missed: {field}"
 
-    @allure.description("This test checks registration of user with names length 1 character")
+    @allure.description("This test checks invalid registration of user with names length 1 character")
     @pytest.mark.parametrize('field', string_fields)
     def test_create_user_with_short_name(self, field):
         data = self.prepare_registration_data()
@@ -64,7 +64,7 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"The value of '{field}' field is too short"
 
-    @allure.description("This test checks registration of user with names length more 250 characters")
+    @allure.description("This test checks invalid registration of user with names length more 250 characters")
     @pytest.mark.parametrize('field', string_fields)
     def test_create_user_with_long_name(self, field):
         data = self.prepare_registration_data()
